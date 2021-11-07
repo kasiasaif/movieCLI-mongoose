191412930fca6e13539a108dc4116ad17e1a399e
kasiasaif <kasia.saif@gmail.com>
2021-11-07 16:01:05 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-require('./db/connection')
-const yargs = require('yargs')
+require('./db/connection');
+const yargs = require('yargs');
 const { addMovie,
     readAll,
+    updateMovie,
+    deleteMovie,
     findByTitle,
     findByActor,
-        updateMovie,
-        deleteMovie,
-        }= require('./movie/movie.methods')
+        }= require('./movie/movie.methods');
 
 const app= () =>{
     if (process.argv[2]=== 'add'){
@@ -21,8 +21,12 @@ const app= () =>{
         updateMovie({title: yargs.argv.title}, {actor: yargs.argv.actor})
     }else if(process.argv[2]=== 'delete'){
         deleteMovie({title: yargs.argv.title, actor: yargs.argv.actor})
+    }else if(process.argv[2]=== 'find by title'){
+        findByTitle({title: yargs.argv.title})
+    }else if(process.argv[2]=== 'find by actor'){
+        findByActor({actor: yargs.argv.actor})
     }else{
-        console.log("Something wrong is happening with ur fingers, you have type incorrect command:)")
+        console.log("Hey focus! You have typed the wrong command")
     }
 }
 
--- src/movie/movie.methods.js
+++ src/movie/movie.methods.js
@@ -9,55 +9,58 @@ exports.addMovie = async (movieObj) => {
         mongoose.disconnect();
         console.log(`${result} successfully added to DB`);
     }catch(error){
-        console.log(error)
+        console.log(error);
     }
-
 };
+
 exports.readAll = async () => {
     try{
         const result = await Movie.find({});
         mongoose.disconnect();
-        console.log('You have search for all movies',result)
+        console.log('You have searched for all movies: ',result);
     }catch(error){
-        console.log(error)
+        console.log(error);
     }
-}
+};
+
+exports.updateMovie = async (movieObj, newMovieObj) => {
+    try{
+        const result = await  Movie.findOneAndUpdate({title: movieObj.title}, {actor: newMovieObj.actor});
+        mongoose.disconnect();
+        console.log(`You have successfully updated movie from: ${result} to actor:  ${newMovieObj.actor}`);
+        
+    }catch(error){
+        console.log(error);
+    }
+};
+
+exports.deleteMovie = async (movieObj) => {
+    try{
+        const result = await  Movie.deleteOne(movieObj);
+        mongoose.disconnect();
+        console.log('Movie has been  deleted.');
+    }catch(error){
+        console.log(error);
+    }
+};
+
 exports.findByTitle = async (movieObj) => {
     try{
         const result = await Movie.find({title: movieObj.title});
         mongoose.disconnect();
-        console.log('You have search for : ',result)
+        console.log('You have search for : ',result);
     }catch(error){
-        console.log(error)
+        console.log(error);
     }
-}
+};
+
 exports.findByActor = async (movieObj) => {
     try{
-        const result = await Movie.find({title: movieObj.title});
+        const result = await Movie.find({actor: movieObj.actor});
         mongoose.disconnect();
-        console.log('You have search for : ',result)
+        console.log('You have search for : ',result);
     }catch(error){
-        console.log(error)
+        console.log(error);
     }
-}
-
-exports.updateMovie = async (actor, newActor) => {
-    try{
-        const result = await  Movie.findOneAndUpdate(actor, newActor)
-        mongoose.disconnect();
-        console.log(`You have  successfully updated from ${result } `)
-        
-    }catch(error){
-        console.log(error)
-    }
-}
-exports.deleteMovie = async (movieObj) => {
-    try{
-        const result = await  Movie.deleteOne(movieObj)
-        mongoose.disconnect();
-        console.log(`You have deleted ${result}`);
-    }catch(error){
-        console.log(error)
-    }
-}
+};
 
