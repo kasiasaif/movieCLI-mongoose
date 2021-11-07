17a38f7b5cbe7d14a42a388a115a58e1b5d6b009
kasiasaif <kasia.saif@gmail.com>
2021-11-07 18:43:17 +0000

    update

--- src/app.js
+++ src/app.js
@@ -13,14 +13,10 @@ const app= () =>{
         addMovie({title: yargs.argv.title, actor: yargs.argv.actor})
     }else if(process.argv[2]=== 'read all'){
         readAll()
-    }else if(process.argv[2]=== 'find by title'){
-        findByTitle({title: yargs.argv.title})
-    }else if(process.argv[2]=== 'find by actor'){
-        findByActor({actor: yargs.argv.actor})
     }else if(process.argv[2]=== 'update'){
         updateMovie({title: yargs.argv.title}, {actor: yargs.argv.actor})
     }else if(process.argv[2]=== 'delete'){
-        deleteMovie({title: yargs.argv.title, actor: yargs.argv.actor})
+        deleteMovie({title: yargs.argv.title})
     }else if(process.argv[2]=== 'find by title'){
         findByTitle({title: yargs.argv.title})
     }else if(process.argv[2]=== 'find by actor'){
